7a95278537647f020621f36071256877841e1c6e
Casey Richardson <Casey.Richardson@jhuapl.edu>
2018-11-15 06:44:38 -0500

    Implements store_sense and store_move

--- rbmc/history.py
+++ rbmc/history.py
@@ -7,14 +7,28 @@ class GameHistory(object):
     def __init__(self):
         self.history = {}
 
+    def __get_history_item_for(self, move_number: int, color: Color):
+        if move_number not in self.history:
+            self.history[move_number] = {}
+        if color not in self.history[move_number]:
+            self.history[move_number][color] = {}
+        return self.history[move_number][color]
+
     def store_sense(self, move_number: int, color: Color, square: Square,
                 sense_result: List[Tuple[Square, Optional[chess.Piece]]]):
-        pass
+        self.__get_history_item_for(move_number, color)["sense"] = {
+            "square": square,
+            "sense_result": sense_result
+        }
 
     def store_move(self, move_number: int, color: Color, 
                 requested_move: chess.Move, taken_move: chess.Move, 
                 opt_capture_square: Square):
-        pass
+        self.__get_history_item_for(move_number, color)["move"] = {
+            "requested_move": requested_move,
+            "taken_move": taken_move,
+            "opt_capture_square": opt_capture_square
+        }
 
     def get_sense_history_for(self, color: Color) -> List[Square]:
         pass
