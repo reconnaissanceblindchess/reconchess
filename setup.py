d324d6f1d8473b470b7d606e03bb56c90b393a3c
Corey Lowman <corey.lowman@jhuapl.edu>
2018-11-19 12:50:11 -0500

    Adding requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+with open('requirements.txt') as fp:
+    requirements = fp.readlines()
+
+setuptools.setup(
+    name='python-rbmc',
+    version='0.1',
+    packages=['rbmc', 'rbmc_server'],
+    package_data={'rbmc': ['res/white/*.png', 'res/black/*.png']},
+    scripts=['scripts/rbmc-bot-match.py', 'scripts/rbmc-connect.py', 'scripts/rbmc-play.py'],
+    install_requires=requirements,
+)
