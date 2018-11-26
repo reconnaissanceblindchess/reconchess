f7a2c0f68303a12f45e4b5654008bbc6c7f2039b
Casey Richardson <Casey.Richardson@jhuapl.edu>
2018-11-26 09:40:31 -0500

    Refactors history: separate lists for move results

--- rbmc/history.py
+++ rbmc/history.py
@@ -7,8 +7,9 @@ class GameHistory(object):
     def __init__(self):
         self.senses = {chess.WHITE: [], chess.BLACK: []}
         self.sense_results = {chess.WHITE: [], chess.BLACK: []}
-        self.moves = {chess.WHITE: [], chess.BLACK: []}
-        self.move_results = {chess.WHITE: [], chess.BLACK: []}
+        self.requested_moves = {chess.WHITE: [], chess.BLACK: []}
+        self.taken_moves = {chess.WHITE: [], chess.BLACK: []}
+        self.capture_squares = {chess.WHITE: [], chess.BLACK: []}
 
     def store_sense(self, color: Color, square: Square,
                 sense_result: List[Tuple[Square, Optional[chess.Piece]]]):
@@ -17,14 +18,12 @@ class GameHistory(object):
 
     def store_move(self, color: Color, requested_move: chess.Move, 
                 taken_move: chess.Move, opt_capture_square: Square):
-        self.moves[color].append(requested_move)
-        self.move_results[color].append({
-            "taken_move": taken_move,
-            "opt_capture_square": opt_capture_square
-        })
+        self.requested_moves[color].append(requested_move)
+        self.taken_moves[color].append(taken_move)
+        self.capture_squares[color].append(opt_capture_square)
 
     def get_sense_history_for(self, color: Color) -> List[Square]:
         return self.senses[color]
 
     def get_move_history_for(self, color: Color) -> List[chess.Move]:
-        return self.moves[color]
+        return self.requested_moves[color]
