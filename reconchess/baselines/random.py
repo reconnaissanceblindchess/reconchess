d77e7b81979ac9fe1d152bf7548bbbc75af29df1
Lowman <lowmaca1@jhuapl.edu>
2019-02-19 10:18:23 -0500

    Changing seconds_left to be the last parameter of choose_sense and choose_move

--- reconchess/baselines/random.py
+++ reconchess/baselines/random.py
@@ -9,13 +9,13 @@ class RandomBot(Player):
     def handle_opponent_move_result(self, captured_my_piece: bool, capture_square: Optional[Square]):
         pass
 
-    def choose_sense(self, seconds_left: float, sense_actions: List[Square], move_actions: List[chess.Move]) -> Square:
+    def choose_sense(self, sense_actions: List[Square], move_actions: List[chess.Move], seconds_left: float) -> Square:
         return random.choice(sense_actions)
 
     def handle_sense_result(self, sense_result: List[Tuple[Square, Optional[chess.Piece]]]):
         pass
 
-    def choose_move(self, seconds_left: float, move_actions: List[chess.Move]) -> Optional[chess.Move]:
+    def choose_move(self, move_actions: List[chess.Move], seconds_left: float) -> Optional[chess.Move]:
         return random.choice(move_actions + [None])
 
     def handle_move_result(self, requested_move: Optional[chess.Move], taken_move: Optional[chess.Move],
