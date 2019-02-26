059b944f0e46cbd7a66eeb443e9b09f7e3d9b4b6
Casey Richardson <Casey.Richardson@jhuapl.edu>
2019-02-26 16:59:03 -0500

    Upping version to 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as fp:
 
 setuptools.setup(
     name='reconchess',
-    version='0.0.1',
+    version='0.0.2',
     author='Johns Hopkins University Applied Physics Laboratory',
     description='A python package for building Reconnaissance Chess players',
     long_description=long_description,
