934e83cd1b969394deda46eddd4621b41ef62017
Lowman <lowmaca1@jhuapl.edu>
2019-03-15 17:16:36 -0400

    Version 0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as fp:
 
 setuptools.setup(
     name='reconchess',
-    version='0.0.2',
+    version='0.0.3',
     author='Johns Hopkins University Applied Physics Laboratory',
     description='A python package for building Reconnaissance Chess players',
     long_description=long_description,
