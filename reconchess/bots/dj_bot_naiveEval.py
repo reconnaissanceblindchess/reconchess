fc48c7ea355386e35d60e3d1e377b0693b55be5b
jatmdm <jatmdm@gmail.com>
2019-05-20 12:22:21 -0400

    Added different versions of the bot based on board evaluation.

--- reconchess/bots/dj_bot_naiveEval.py
+++ reconchess/bots/dj_bot_naiveEval.py
@@ -4,7 +4,7 @@ from reconchess import *
 import os
 # from stockfish import Stockfish
 
-class DJBot(Player):
+class DJBotNaiveEval(Player):
 
     # GENERAL STRATEGY:
 
@@ -340,7 +340,7 @@ class DJBot(Player):
         for move in move_actions:
             test_board = board
             test_board.push(move)
-            cur_score = self.evaluateBoard(test_board)
+            cur_score = self.naiveEvaluation(test_board)
             if best_score < cur_score:
                     best_score = cur_score
                     best_move = move
