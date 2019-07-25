1c101e2dd58064c9b41c736a767d34a078fab70c
Corey Lowman <corey.lowman@jhuapl.edu>
2019-07-25 11:20:09 -0400

    Retrying request if receive a 502, and raising error if we don't receive 200

--- reconchess/game.py
+++ reconchess/game.py
@@ -345,11 +345,21 @@ class RemoteGame(Game):
 
     def _get(self, endpoint, decoder_cls=ChessJSONDecoder):
         response = self.session.get('{}/{}'.format(self.game_url, endpoint))
+        while response.status_code == 502:
+            time.sleep(0.5)
+            response = self.session.get('{}/{}'.format(self.game_url, endpoint))
+        if response.status_code != 200:
+            raise ValueError(response.content)
         return response.json(cls=decoder_cls)
 
     def _post(self, endpoint, obj):
         data = json.dumps(obj, cls=ChessJSONEncoder)
         response = self.session.post('{}/{}'.format(self.game_url, endpoint), data=data)
+        while response.status_code == 502:
+            time.sleep(0.5)
+            response = self.session.post('{}/{}'.format(self.game_url, endpoint), data=data)
+        if response.status_code != 200:
+            raise ValueError(response.content)
         return response.json(cls=ChessJSONDecoder)
 
     def get_player_color(self):
