40b3f022fe7b8cc00d22e0fa0221dae9f7d7c972
Lowman, Corey A <corey.lowman@jhuapl.edu>
2019-07-31 11:48:55 -0400

    Version 1.4.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as fp:
 
 setuptools.setup(
     name='reconchess',
-    version='1.3.5',
+    version='1.4.0',
     author='Johns Hopkins University Applied Physics Laboratory',
     description='A python package for building Reconnaissance Chess players',
     long_description=long_description,
