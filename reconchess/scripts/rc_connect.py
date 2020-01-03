10cefecbf7f050fedde431806132d07eb7d6a00e
Corey Lowman <corey.lowman@jhuapl.edu>
2020-01-03 09:52:57 -0500

    Adding ranked modes & version update to rc-connect

--- reconchess/scripts/rc_connect.py
+++ reconchess/scripts/rc_connect.py
@@ -68,6 +68,15 @@ class RBCServer:
     def error_resign(self, game_id):
         return self._post('{}/{}/error_resign'.format(self.game_url, game_id))
 
+    def set_ranked(self, ranked):
+        self._post('{}/ranked'.format(self.me_url), json={'ranked': ranked})
+
+    def get_bot_version(self):
+        return self._get('{}/version'.format(self.me_url))['version']
+
+    def increment_version(self):
+        self._post('{}/version'.format(self.me_url))
+
 
 def accept_invitation_and_play(server_url, auth, invitation_id, bot_cls):
     print('[{}] Accepting invitation {}.'.format(datetime.now(), invitation_id))
@@ -88,9 +97,7 @@ def accept_invitation_and_play(server_url, auth, invitation_id, bot_cls):
         server.finish_invitation(invitation_id)
 
 
-def listen_for_invitations(server_url, auth, bot_cls, max_concurrent_games):
-    server = RBCServer(server_url, auth)
-
+def check_package_version(server):
     server_version = server.get_reconchess_version()
     if server_version != reconchess.__version__:
         print('Your reconchess package is out of date!'.format(reconchess.__version__))
@@ -99,6 +106,35 @@ def listen_for_invitations(server_url, auth, bot_cls, max_concurrent_games):
         print('Please run the command `pip install --upgrade reconchess` to upgrade to the latest.')
         quit()
 
+
+def ranked_mode(server):
+    current_version = server.get_bot_version()
+
+    # ask if they want to update their version number
+    if current_version == 0:
+        should_increment_version = True
+    else:
+        should_increment_version = confirm('Is this a new version of your bot?')
+
+    # compute the new version depending on their answer
+    next_version = current_version + (1 if should_increment_version else 0)
+
+    # make sure they want to participate with this info
+    if not confirm('Are you sure you want to participate in ranked matches as v{} (currently v{})?'.format(
+            next_version, current_version)):
+        quit()
+
+    # update information on server
+    if should_increment_version:
+        server.increment_version()
+    server.set_ranked(True)
+
+
+def unranked_mode(server):
+    server.set_ranked(False)
+
+
+def listen_for_invitations(server, bot_cls, max_concurrent_games):
     connected = False
     process_by_invitation = {}
     while True:
@@ -123,7 +159,8 @@ def listen_for_invitations(server_url, auth, bot_cls, max_concurrent_games):
 
                     # start the process for playing a game
                     process = multiprocessing.Process(
-                        target=accept_invitation_and_play, args=(server_url, auth, invitation, bot_cls))
+                        target=accept_invitation_and_play,
+                        args=(server.server_url, server.session.auth, invitation, bot_cls))
                     process.start()
 
                     # store the process so we can check when it finishes
@@ -152,12 +189,25 @@ def ask_for_auth():
     return ask_for_username(), ask_for_password()
 
 
+def confirm(prompt):
+    while True:
+        response = input(prompt + ' [y/n]')
+        if len(response) == 0 or response.lower()[0] == 'y':
+            return True
+        elif len(response) > 0 and response.lower()[0] == 'n':
+            return False
+        else:
+            print('Invalid input... please enter one of {"y", "n", "yes", "no", <Enter>, ...}')
+
+
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('bot_path', help='Path to bot source or bot module name.')
     parser.add_argument('--username', default=None, help='Username for login. Enter with prompt if not specified.')
     parser.add_argument('--password', default=None, help='Password for login. Enter with prompt if not specified.')
     parser.add_argument('--server-url', default='https://rbc.jhuapl.edu', help='URL of the server.')
+    parser.add_argument('--ranked', action='store_true', default=False,
+                        help='Whether you want to play ranked matches.')
     parser.add_argument('--max-concurrent-games', type=int, default=4,
                         help='The maximum number of games to play at the same time.')
     args = parser.parse_args()
@@ -168,7 +218,18 @@ def main():
     password = ask_for_password() if args.password is None else args.password
     auth = username, password
 
-    listen_for_invitations(args.server_url, auth, bot_cls, args.max_concurrent_games)
+    server = RBCServer(args.server_url, auth)
+
+    # verify we have the correct version of reconchess package
+    check_package_version(server)
+
+    # tell the server whether we want to do ranked matches or not
+    if args.ranked:
+        ranked_mode(server)
+    else:
+        unranked_mode(server)
+
+    listen_for_invitations(server, bot_cls, args.max_concurrent_games)
 
 
 if __name__ == '__main__':
